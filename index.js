3725b2a9d75a7bbca58c384e5f20060c5455a6b4
MadLar <pjcj1@naver.com>
2022-01-21 15:44:29 +0900

    mongoDB 연결

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://madlar:1q2w3e4r!@mygg.lemu1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+}).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
