df6cc2621ea0723aa655b1b5e48c417c3e362312
MadLar <pjcj1@naver.com>
2022-02-26 13:21:40 +0900

    NavBar 수정

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -38,31 +38,29 @@ function NavBar() {
   if(isHome) {//현재 주소가 렌딩페이지이면 검색창을 없앰
     return (
       <div>
-    {/* A "layout route" is a good place to put markup you want to
+      {/* A "layout route" is a good place to put markup you want to
         share across all the pages on your site, like navigation. */}
-    <nav style={{display: 'flex', alignItems: 'center' }}>
-      <div style={{display: 'flex', width:'1000px', float:'left'}}>
-      <Menu mode='horizontal'>
-        <Menu.Item key="home">
-          <Link to="/">홈</Link>
-        </Menu.Item>
-        <Menu.Item key="champion">
-          <Link to="/champion">챔피언</Link>
-        </Menu.Item>
-        <Menu.Item key="arma">
-        <Link to="/arma">칼바람</Link>
-        </Menu.Item>
-      </Menu>
-      </div>
-    </nav>
+        <nav style={{ display: 'flex', alignItems: 'center' }}>
+          <div style={{ width:'1000px' }}>
+            <Menu mode='horizontal'>
+              <Menu.Item key="home">
+                <Link to="/">홈</Link>
+              </Menu.Item>
+              <Menu.Item key="champion">
+                <Link to="/champion">챔피언</Link>
+              </Menu.Item>
+              <Menu.Item key="arma">
+                <Link to="/arma">칼바람</Link>
+              </Menu.Item>
+            </Menu>
+          </div>
+        </nav>
 
-    <hr />
-
-    {/* An <Outlet> renders whatever child route is currently active,
+      {/* An <Outlet> renders whatever child route is currently active,
         so you can think about this <Outlet> as a placeholder for
         the child routes we defined above. */}
-    <Outlet />
-  </div>
+        <Outlet />
+      </div>
     )
   }
 
@@ -70,41 +68,40 @@ function NavBar() {
     <div>
     {/* A "layout route" is a good place to put markup you want to
         share across all the pages on your site, like navigation. */}
-    <nav style={{display: 'flex', alignItems: 'center' }}>
-      <div style={{display: 'flex', width:'1000px', float:'left'}}>
-      <Menu mode='horizontal'>
-        <Menu.Item key="home">
-          <Link to="/">홈</Link>
-        </Menu.Item>
-        <Menu.Item key="champion">
-          <Link to="/champion">챔피언</Link>
-        </Menu.Item>
-        <Menu.Item key="arma">
-        <Link to="/arma">칼바람</Link>
-        </Menu.Item>
-      </Menu>
-      </div>
-      <div style={{ display: 'flex', float: 'right', marginRight: '0'}}>
-        <div style={{ display: 'flex', width:'300px' }}>
-          <Search 
-            placeholder="소환사명"
-            enterButton="Search"
-            size="medium"
-            value={Name}
-            onChange={onNameHandler}
-            onSearch={onSearchHandler}
-          />
+      <nav style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ width:'1000px' }}>
+          <Menu mode='horizontal'>
+            <Menu.Item key="home">
+              <Link to="/">홈</Link>
+            </Menu.Item>
+            <Menu.Item key="champion">
+              <Link to="/champion">챔피언</Link>
+            </Menu.Item>
+            <Menu.Item key="arma">
+              <Link to="/arma">칼바람</Link>
+            </Menu.Item>
+          </Menu>
         </div>
-      </div>
-    </nav>
+        <div style={{}}>
+          <div style={{ width:'300px' }}>
+            <Search 
+              placeholder="소환사명"
+              enterButton="Search"
+              size="medium"
+              value={Name}
+              onChange={onNameHandler}
+              onSearch={onSearchHandler}
+            />
+          </div>
+        </div>
+      </nav>
 
-    <hr />
-
-    {/* An <Outlet> renders whatever child route is currently active,
+      {/* An <Outlet> renders whatever child route is currently active,
         so you can think about this <Outlet> as a placeholder for
         the child routes we defined above. */}
-    <Outlet />
-  </div>);
+      <Outlet />
+    </div>
+  );
 }
 
 export default NavBar;
