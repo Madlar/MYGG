c94607ee3be890d97fcbf48ffe7bfa7dd03d03cb
MadLar <pjcj1@naver.com>
2022-03-14 23:54:57 +0900

    1.0

--- src/components/views/SummonerPage/LeagueEntry.js
+++ src/components/views/SummonerPage/LeagueEntry.js
@@ -6,6 +6,38 @@ function LeagueEntry(props) {
     const loss = props.leagueEntry.losses
     const winRate = Math.round(win / (win+loss) * 100)
 
+    const tierImg = () => {
+        switch (props.leagueEntry.tier) {
+            case 'BRONZE':
+                return 'Emblem_Bronze.png'
+                break;
+            case 'CHALLENGER':
+                return 'Emblem_Challenger.png'
+                break;
+            case 'DIAMOND':
+                return 'Emblem_Diamond.png'
+                break;
+            case 'GOLD':
+                return 'Emblem_Gold.png'
+                break;
+            case 'IRON':
+                return 'Emblem_Iron.png'
+                break;
+            case 'MASTER':
+                return 'Emblem_Master.png'
+                break;
+            case 'PLATINUM':
+                return 'Emblem_Platinum.png'
+                break;
+            case 'SILVER':
+                return 'Emblem_Silver.png'
+                break;
+            default:
+                return 'provisional.png'
+                break;
+        }
+    }
+
     if(props.leagueEntry == 'UNRANKED') {
         return(
             <Card style={{ width: '300px', height: '150px', display: 'flex' }}>
@@ -34,7 +66,7 @@ function LeagueEntry(props) {
                 <div style={{ display: 'flex' }}>
                     <div>
                         <img width="100" height="100" 
-                        src={ `${process.env.PUBLIC_URL}/ranked-emblems/Emblem_${props.leagueEntry.tier}.png` } />
+                        src={ `${process.env.PUBLIC_URL}/ranked-emblems/${tierImg()}` } />
                     </div>
                     <div style={{ marginLeft: '20px' }}>
                         <div style={{ float: 'left' }}>
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     gameVersion: '12.5.1',
-    languages: 'ko_KR'
+    languages: 'ko_KR',
+    api_server: 'http://ec2-3-35-234-120.ap-northeast-2.compute.amazonaws.com:5000'
 }
